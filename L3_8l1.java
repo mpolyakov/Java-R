95ff1d437ea6423075a21ea6b211325e97c39d95
mpolyakov <polyakov4@yandex.ru>
2019-05-28 23:06:00 +0300

    one more task from javarush

---
+++ L3_8l1.java
@@ -0,0 +1,18 @@
+﻿import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //�������� ��� ��� ���
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        
+        String name = bufferedReader.readLine();
+        String amount = bufferedReader.readLine();
+        int nAmount = Integer.parseInt(amount);
+        String age = bufferedReader.readLine();
+        int nAge = Integer.parseInt(age);
+        
+        System.out.println(name + " �������� " + nAmount + " ����� " + nAge + " ���.");
+    }
+}
