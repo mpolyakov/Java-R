af7a814928a6c9f80055b8cb591bb16a4a612121
mpolyakov <polyakov4@yandex.ru>
2019-09-03 23:49:37 +0300

    some new tasks from JavaRush

---
+++ L12_l4_2.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(3);
+        print((Integer)5);
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int a){
+        System.out.println("This is int");
+    }
+    
+    public static void print(Integer b){
+        System.out.println("This is Integer");
+        
+    }
+}
---
+++ L12_l4_3.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String str){
+        
+    }
+    
+    public static void print(String str, int a){
+        
+    }
+    
+    public static void print(char c){
+        
+    }
+    
+    public static void print(Character y){
+        
+    }
+    
+    public static void print(char c, int t, String s){
+        
+    }
+}
