2246a31bf642acc2110d8450e475936eece5b1a5
mpolyakov <polyakov4@yandex.ru>
2019-09-07 22:58:08 +0300

    three new tasks from JavaRush

---
+++ L12_l9_2.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+    public interface CanFly{
+        public void itFlies();
+    }
+    
+    public interface CanRun{
+        public void itRuns();
+    }
+    
+    public interface CanSwim{
+        public void itSwims();
+    }
+
+}
