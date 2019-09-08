6221464bb97a05631bcb4e820398c36dececbfd0
mpolyakov <polyakov4@yandex.ru>
2019-09-08 22:56:36 +0300

    new tasks from JavaRush

---
+++ L12_l9_4.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task12.task1219;
+
+/* 
+Создаем человека
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+
+    public class Human implements CanRun, CanSwim {
+        public void run(){
+            
+        }
+        public void swim(){
+            
+        }
+
+    }
+
+    public class Duck implements CanRun, CanFly, CanSwim {
+        public void run(){
+            
+        }
+        public void fly(){
+            
+        }
+        public void swim(){
+            
+        }
+
+    }
+
+    public class Penguin implements CanSwim, CanRun{
+        public void run(){
+            
+        }
+        public void swim(){
+            
+        }
+
+    }
+
+    public class Airplane implements CanFly, CanRun{
+        public void run(){
+            
+        }
+        public void fly(){
+            
+        }
+
+    }
+}
