a7dd915e837c8131842f398dbe532c68a351b99a
mpolyakov <polyakov4@yandex.ru>
2019-10-20 22:30:20 +0300

    some new tasks from JavaRush

---
+++ L15_l12_5_1.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    
+    public Solution(String name) {
+        
+    }
+    private Solution(int age) {
+        
+    }
+    protected Solution(String name, int height) {
+        
+    }
+    Solution() {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ L15_l12_5_2.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+
+public class SubSolution extends Solution {
+    public SubSolution(String name) {
+        
+    }
+    protected SubSolution(String name, int height) {
+        
+    }
+    SubSolution() {
+        
+    }
+}
